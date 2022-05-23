edac2000e096ad4ff8cdf2f4e13b779022094428
Marie Howet <howet.marie@gmail.com>
2022-05-23 14:54:50 +0200

    Phase 4  : DA + code

--- src/classificationPerformances.c
+++ src/classificationPerformances.c
@@ -2,8 +2,8 @@
 #include <stdio.h> 
 #include <stdbool.h>
 #include < stdlib.h>
-#define NB_TESTS 8
-#define NB_CLASSES 5
+#define NB_TESTS 31
+#define NB_CLASSES 6
 
 void displayResultsByClass(int realClasses[], int estimatedClasses[], int confusionMatrix[][NB_CLASSES]);
 void displayAccuracy(int realClasses[], int estimatedClasses[], int confusionMatrix[][NB_CLASSES]);
@@ -17,56 +17,6 @@ void main(void) {
 	int confusionMatrix[NB_CLASSES][NB_CLASSES] = { 0 };
 	createConfusionMatrix(realClasses, estimatedClasses, confusionMatrix);
 	displayResultsByClass(realClasses, estimatedClasses, confusionMatrix);
-	displayAccuracy(realClasses, estimatedClasses, confusionMatrix);
+	displayAccuracy(confusionMatrix);
 	displayConfusionMatrix(confusionMatrix);
-}
-
-void displayResultsByClass(int realClasses[], int estimatedClasses[], int confusionMatrix[][NB_CLASSES]) {
-	printf("Classes\tBien classes\tTotal\tPourcentage\n");
-	int sum;
-	for (int i = 0; i < NB_CLASSES; i++) {
-		sum = 0;
-		for (int j = 0; j < NB_CLASSES; j++)
-			sum += confusionMatrix[i][j];
-		printf("%d\t\t%d\t%d\t%.2f%%\n", i + 1, confusionMatrix[i][i], sum, ((double)confusionMatrix[i][i] / sum) * 100);
-	}
-}
-
-void displayAccuracy(int realClasses[], int estimatedClasses[], int confusionMatrix[][NB_CLASSES]) {
-	int sumWP = 0;
-	int sumRP = 0;
-	for (int i = 0; i < NB_CLASSES; i++) {
-		for (int j = 0; j < NB_CLASSES; j++) {
-			if (i == j && confusionMatrix[i][j] >= 1)
-				sumWP += confusionMatrix[i][j];
-			sumRP += confusionMatrix[i][j];
-		}
-	}
-	printf("The accuracy is %.2f %%\n", (double)sumWP / sumRP * 100);
-}
-
-void displayConfusionMatrix(int confusionMatrix[][NB_CLASSES]) {
-	for (int i = 0; i < NB_CLASSES; i++)
-		printf("\t%d", (i + 1));
-	printf("\n__________________________________________________\n");
-	for (int i = 0; i < NB_CLASSES; i++) {
-		printf("%d |", (i + 1));
-		for (int j = 0; j < NB_CLASSES; j++) {
-			printf("\t%d", confusionMatrix[i][j]);
-		}
-		printf("\n");
-	}
-}
-
-void createConfusionMatrix(int realClasses[], int estimatedClasses[], int confusionMatrix[][NB_CLASSES]) {
-	for (int iTest = 0; iTest < NB_TESTS; iTest++) {
-		int i = 1;
-		while (i <= NB_CLASSES && i != estimatedClasses[iTest])
-			i++;
-		int j = 1;
-		while (j <= NB_CLASSES && j != realClasses[iTest])
-			j++;
-		if (i <= NB_CLASSES)
-			confusionMatrix[i - 1][j - 1]++;
-	}
 }
--- src/classificationPerformances.h
+++ src/classificationPerformances.h
@@ -3,12 +3,63 @@
 #include <stdio.h> 
 #include <stdbool.h>
 #include < stdlib.h>
-#define NB_TESTS 8
-#define NB_CLASSES 5
+#define NB_TESTS 30
+#define NB_CLASSES 6
 
-void displayResultsByClass(int realClasses[], int estimatedClasses[], int confusionMatrix[][NB_CLASSES]);
-void displayAccuracy(int realClasses[], int estimatedClasses[], int confusionMatrix[][NB_CLASSES]);
-void displayConfusionMatrix(int confusionMatrix[][NB_CLASSES]);
-void createConfusionMatrix(int realClasses[], int estimatedClasses[], int confusionMatrix[][NB_CLASSES]);
+void displayResultsByClass(int realClasses[], int estimatedClasses[], int confusionMatrix[][NB_CLASSES]) {
+	printf("Classes\tBien classes\tTotal\tPourcentage\n");
+	int sum;
+	double percent;
+	for (int iColumn = 0; iColumn < NB_CLASSES; iColumn++) {
+		sum = 0;
+		for (int iLine = 0; iLine < NB_CLASSES; iLine++)
+			sum += confusionMatrix[iLine][iColumn];
+		if (sum == 0)
+		percent = 0;
+			else 
+		percent = ((double)confusionMatrix[iColumn][iColumn] / sum) * 100;
+		printf("%d\t\t%d\t%d\t%.2f%%\n", iColumn + 1, confusionMatrix[iColumn][iColumn], sum, percent);
+	}
+}
+
+void displayAccuracy(int confusionMatrix[][NB_CLASSES]) {
+	int sumWP = 0;
+	int total = 0;
+	for (int i = 0; i < NB_CLASSES; i++) {
+		for (int j = 0; j < NB_CLASSES; j++) {
+			if (i == j)
+				sumWP += confusionMatrix[i][j];
+			total += confusionMatrix[i][j];
+		}
+	}
+	printf("The accuracy is %.2f %%\n", (double)sumWP / total * 100);
+}
+
+void displayConfusionMatrix(int confusionMatrix[][NB_CLASSES]) {
+	for (int i = 0; i < NB_CLASSES; i++)
+		printf("\t%d", (i + 1));
+	printf("\n__________________________________________________\n");
+	for (int i = 0; i < NB_CLASSES; i++) {
+		printf("%d |", (i + 1));
+		for (int j = 0; j < NB_CLASSES; j++) {
+			printf("\t%d", confusionMatrix[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void createConfusionMatrix(int realClasses[], int estimatedClasses[], int confusionMatrix[][NB_CLASSES]) {
+	for (int iTest = 0; iTest < NB_TESTS; iTest++) {
+		int i = 1;
+		while (i <= NB_CLASSES && i != estimatedClasses[iTest])
+			i++;
+		int j = 1;
+		while (j <= NB_CLASSES && j != realClasses[iTest])
+			j++;
+		if (i <= NB_CLASSES)
+			confusionMatrix[i - 1][j - 1]++;
+	}
+}
+
 #endif
 
