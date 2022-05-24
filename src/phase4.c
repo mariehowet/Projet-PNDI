926f00c37e5bff0d05e8e5eba24de8fde0a74cbe
Marie Howet <69237293+mariehowet@users.noreply.github.com>
2022-05-24 16:46:03 +0200

    Update phase4.c

--- src/phase4.c
+++ src/phase4.c
@@ -18,8 +18,8 @@
 #define NB_LINES_MATRIX_FIMODEL 2
 #define NB_LINES_FISET 30
 
-typedef struct testLine TestLine;
-struct testLine {
+typedef struct file File;
+struct file {
     int movement;
     double vAccs[NB_VACCS];
 };
@@ -39,14 +39,14 @@ int compareDistances(double distances[], Model model[]);
 int compareGlobalAvg(double sumGlobalAvg, int totVAccs, Model model[]);
 int estimatedClass(int indicator1, int indicator2, int indicator3);
 void fillModel(FILE* fpFiModel, Model model[]);
-TestLine fillTestLine(FILE* fpTest);
+File fillFile(char line[]);
 
 
 void main(void) {
     FILE* fpTest;
     FILE* fpFiModel;
     char line[LINE];
-    TestLine testLine;
+    File file;
     int realClasses[NB_LINES_FISET];
     int estimatedClasses[NB_LINES_FISET];
     int confusionMatrix[NB_MOVEMENTS][NB_MOVEMENTS] = { 0 };
@@ -60,71 +60,73 @@ void main(void) {
     int indicateur1;
     int indicateur2;
     int indicateur3;
- 
+
 
     fopen_s(&fpTest, FILE_TEST, "r");
     fopen_s(&fpFiModel, FILE_FIMODEL, "r");
 
     if (fpTest == NULL || fpFiModel == NULL) {
-    puts("error opening trainSet or fiModel");
-      } else {
-            fillModel(fpFiModel, model);
-            iClass = 0;
-            fgets(line, LINE, fpTest); // headers
-            while (!feof(fpTest) && iClass < NB_LINES_FISET) {
-                testLine = fillTestLine(fpTest);
-                initArray(distancesAvgs);
-                initArray(distancesStds);
-                sumGlobalAvg = 0;
-                totVaccs = 0;
-
-                for (int iMovement = 0; iMovement < NB_MOVEMENTS; iMovement++)
-                {
-                    for (int iVacc = 0; iVacc < NB_VACCS; iVacc++) {
-                        sumGlobalAvg += testLine.vAccs[iVacc];
-                        distancesAvgs[iMovement] += pow(testLine.vAccs[iVacc] - model[iMovement].averages[iVacc], 2);
-                        lineStd = sqrt(pow(testLine.vAccs[iVacc] - model[iMovement].averages[iVacc], 2)); //(xi - moyenne)²( / 1)
-                        distancesStds[iMovement] += pow(lineStd - model[iMovement].stds[iVacc], 2);
-                        totVaccs++;
-                    }
-                }
-
-                for (int iDistance = 0; iDistance < NB_MOVEMENTS; iDistance++) {
-                    distancesAvgs[iDistance] = sqrt(distancesAvgs[iDistance]);
-                    distancesStds[iDistance] = sqrt(distancesStds[iDistance]);
-                }
-
-                indicateur1 = compareDistances(distancesAvgs, model);
-                indicateur2 = compareDistances(distancesStds, model);
-                indicateur3 = compareGlobalAvg(sumGlobalAvg, totVaccs, model);
-                estimatedClasses[iClass] = estimatedClass(indicateur1, indicateur2, indicateur3);
-                realClasses[iClass] = testLine.movement;
-                iClass++;
-               
-            }
-            
-            fclose(fpFiModel);
-            fclose(fpTest);
-
-            createConfusionMatrix(realClasses, estimatedClasses, confusionMatrix);
-            printf("Legende \n %d : downstairs, %d : jogging, %d : sitting, %d : standing, %d : upstairs, %d : walking \n",1,2,3,4,5,6);
-            displayResultsByClass(realClasses, estimatedClasses, confusionMatrix);
-            displayAccuracy(confusionMatrix);
-            displayConfusionMatrix(confusionMatrix);
+        puts("error opening trainSet or fiModel");
     }
-      
+    else {
+        fillModel(fpFiModel, model);
+        iClass = 0;
+        fgets(line, LINE, fpTest); // headers
+        while (!feof(fpTest) && iClass < NB_LINES_FISET) {
+            fgets(line, LINE, fpTest);
+            file = fillFile(line);
+            initArray(distancesAvgs);
+            initArray(distancesStds);
+            sumGlobalAvg = 0;
+            totVaccs = 0;
+
+            for (int iMovement = 0; iMovement < NB_MOVEMENTS; iMovement++)
+            {
+                for (int iVacc = 0; iVacc < NB_VACCS; iVacc++) {
+                    sumGlobalAvg += file.vAccs[iVacc];
+                    distancesAvgs[iMovement] += pow(file.vAccs[iVacc] - model[iMovement].averages[iVacc], 2);
+                    lineStd = sqrt(pow(file.vAccs[iVacc] - model[iMovement].averages[iVacc], 2)); //(xi - moyenne)²( / 1)
+                    distancesStds[iMovement] += pow(lineStd - model[iMovement].stds[iVacc], 2);
+                    totVaccs++;
+                }
+            }
+
+            for (int iDistance = 0; iDistance < NB_MOVEMENTS; iDistance++) {
+                distancesAvgs[iDistance] = sqrt(distancesAvgs[iDistance]);
+                distancesStds[iDistance] = sqrt(distancesStds[iDistance]);
+            }
+
+            indicateur1 = compareDistances(distancesAvgs, model);
+            indicateur2 = compareDistances(distancesStds, model);
+            indicateur3 = compareGlobalAvg(sumGlobalAvg, totVaccs, model);
+            estimatedClasses[iClass] = estimatedClass(indicateur1, indicateur2, indicateur3);
+            realClasses[iClass] = file.movement;
+            iClass++;
+
+        }
+
+        fclose(fpFiModel);
+        fclose(fpTest);
+
+        createConfusionMatrix(realClasses, estimatedClasses, confusionMatrix);
+        printf("Legende \n %d : downstairs, %d : jogging, %d : sitting, %d : standing, %d : upstairs, %d : walking \n", 1, 2, 3, 4, 5, 6);
+        displayResultsByClass(realClasses, estimatedClasses, confusionMatrix);
+        displayAccuracy(confusionMatrix);
+        displayConfusionMatrix(confusionMatrix);
+    }
+
 }
 
-void fillModel(FILE* fpFiModel,Model model[])
+void fillModel(FILE* fpFiModel, Model model[])
 {
     char line[LINE];
     char* token;
-   
+
     fgets(line, LINE, fpFiModel); // headers
     int iModel = 0;
     while (iModel < NB_MOVEMENTS) {
         fgets(line, LINE, fpFiModel);
-        token = strtok(line, ","); 
+        token = strtok(line, ",");
         model[iModel].numMotion = atoi(token);
         int iAvg = 0;
         while (token != NULL && iAvg < NB_VACCS) {
@@ -134,7 +136,7 @@ void fillModel(FILE* fpFiModel,Model model[])
         }
 
         fgets(line, LINE, fpFiModel);
-        token = strtok(line, ","); 
+        token = strtok(line, ",");
         token = strtok(NULL, ",");
         int iStd = 0;
         while (token != NULL && iStd < NB_VACCS) {
@@ -152,25 +154,24 @@ void fillModel(FILE* fpFiModel,Model model[])
 }
 
 
-TestLine fillTestLine(FILE* fpTest) {
-    TestLine testLine;
-    char line[LINE];
+File fillFile(char line[]) {
+    File file;
     char* token;
-    fgets(line, LINE, fpTest);
     token = strtok(line, ",");
-    testLine.movement = atoi(token);
+    file.movement = atoi(token);
     token = strtok(NULL, ",");
     token = strtok(NULL, ",");
-
     token = strtok(NULL, ",");
     int i = 0;
     while (token != NULL && i < NB_VACCS) {
-        testLine.vAccs[i] = atof(token);
+        file.vAccs[i] = atof(token);
         token = strtok(NULL, ",");
         i++;
     }
-    return testLine;
+
+    return file;
 }
+
 
 void initArray(double array[]) {
     for (int i = 0; i < NB_MOVEMENTS; i++) {
@@ -198,7 +199,7 @@ int compareGlobalAvg(double sumGlobalAvg, int totVAccs, Model model[]) {
     double distances[NB_MOVEMENTS];
     for (int i = 0; i < NB_MOVEMENTS; i++)
     {
-        distances[i] =  sqrt(pow(globalAverage - model[i].globalAvg, 2));
+        distances[i] = sqrt(pow(globalAverage - model[i].globalAvg, 2));
     }
     int iMin = indMinValue(distances);
     return model[iMin].numMotion;
