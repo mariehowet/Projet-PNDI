7b54da1ad0ef6dd05d6acb79ff5cbdefce49cd7b
Marie Howet <69237293+mariehowet@users.noreply.github.com>
2022-05-24 18:47:11 +0200

    Update phase2.c

--- src/phase2.c
+++ src/phase2.c
@@ -1,10 +1,10 @@
 #include "classificationPerformances.h"
-
+#define NB_TESTS 8
 void main(void) {
 	int realClasses[] = { 5, 2, 5, 3, 1, 3, 2, 4};
 	int estimatedClasses[] = { 5, 5, 1, 2, 1, 3, 2, 4};
 	int confusionMatrix[NB_CLASSES][NB_CLASSES] = { 0 };
-	createConfusionMatrix(realClasses, estimatedClasses, confusionMatrix);
+	createConfusionMatrix(realClasses, estimatedClasses, confusionMatrix, NB_TESTS);
 	displayResultsByClass(confusionMatrix);
 	displayAccuracy(confusionMatrix);
 	displayConfusionMatrix(confusionMatrix);
